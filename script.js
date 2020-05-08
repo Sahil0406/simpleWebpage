2854de6ac156e9d02f2758380d16c74e085c4935
Sahil0406 <65025183+Sahil0406@users.noreply.github.com>
2020-05-08 19:51:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+var num = 1;
+function add() {
+    num = num + 1;
+    
+    document.getElementById("num")
+    .innerHTML = num ;
+    
+    
+}
+
+function min() {
+    num = num - 1;
+
+    document.getElementById("num")
+    .innerHTML = num;
+}
